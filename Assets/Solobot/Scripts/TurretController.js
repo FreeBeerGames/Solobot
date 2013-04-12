d52a81b1026d2d86e0c0b07b8a81f1b80bd29ac3
CoryG89 <CoryG89@gmail.com>
2013-04-12 15:08:24 -0500

    Adding turret prefab and cleaning up directory.

---
+++ Assets/Solobot/Scripts/TurretController.js
@@ -0,0 +1,13 @@
+#pragma strict
+
+public var turretArmMoveSpeed : float = 20.0;
+public var weaponTransform : Transform;
+public var target : Transform;
+
+function Start () {
+
+}
+
+function LateUpdate () {
+	
+}
